84edfedc719a4609bd920a674ffa3b19474ef0ad
saikumar2564 <saikumar8143138350@gmail.com>
2023-08-28 11:46:21 +0530

    login and signuppages

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const {db}=require("./Config/db")
 const cors = require("cors")
-const lawyerroute = require("./Routes/lawyer.routes")
+// const lawyerroute = require("./Routes/lawyer.routes")
 const LawyerRoutes=require("./Routes/LawyerControllar")
 require("dotenv").config()
 const port = process.env.PORT
@@ -10,8 +10,12 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 const { BookingRoute } = require("./Routes/booking.route")
+const { userrouter } = require("./Routes/user.route")
 app.use("/getLawyer", LawyerRoutes)
-
+app.use("/user",userrouter)
+app.get("/",(req,res)=>{
+    res.send("hi")
+})
 app.listen(port, () => {
     try {
         db()
